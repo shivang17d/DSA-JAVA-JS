d4db5241d0d178f6a636a51689a2a586bb1ad23f
Shivang Gupta <shivangguptaxd@gmail.com>
2022-03-25 19:59:57 +0530

    initial commit

--- LEETCODE/Arrays/1.2 Best Time to Buy and Sell Stock/main.js
+++ LEETCODE/Arrays/1.2 Best Time to Buy and Sell Stock/main.js
@@ -1,6 +1,7 @@
 var maxProfit = function(prices) {
     let min_price = Number.MAX_VALUE;
     let profit = 0;
+    
     for(let i = 0 ; i < prices.length ; i++){
         min_price = Math.min(prices[i],min_price);
         profit = Math.max(profit, prices[i] - min_price);
