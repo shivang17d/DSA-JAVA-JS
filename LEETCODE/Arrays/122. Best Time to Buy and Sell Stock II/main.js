0488af2c5b4049c699a8645a14dda041798fa700
Shivang Gupta <shivangguptaxd@gmail.com>
2022-03-25 20:15:27 +0530

    initial commit

--- LEETCODE/Arrays/122. Best Time to Buy and Sell Stock II/main.js
+++
@@ -1,9 +0,0 @@
-var maxProfit = function(prices) {
-    let net_profit = 0;
-    for(let i = 1; i < prices.length ; i++){
-        if(prices[i] > prices[i-1]){
-            net_profit += prices[i] - prices[i-1];
-        }
-    }
-    return net_profit;
-};
